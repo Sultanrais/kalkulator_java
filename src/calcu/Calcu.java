5a74f988c3cab1585d6cd5c9c3b8cd8d5693cb8e
Sultan Rais <sultanrais122@gmail.com>
2025-02-15 11:04:19 +0700

    first commit

---
+++ src/calcu/Calcu.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package calcu;
+
+/**
+ *
+ * @author USER
+ */
+public class Calcu {
+    String angka;
+double jumlah,bil1,bil2;
+int pilih;
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
